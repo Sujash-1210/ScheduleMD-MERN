75186316ccd9898ccff5d78cfe55d300dbc27355
Sujash-1210 <sujashdas80@gmail.com>
2025-02-28 22:14:27 +0530

    fix doctorcontext issue

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://doctor-app-backend-3vi8.onrender.com";
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
